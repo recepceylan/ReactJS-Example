6e00527c08e46f6d67ffcb9346f478a9dda61cd2
Recep Ceylan <recepceylan.09@gmail.com>
2022-03-14 16:07:41 +0300

    NavbarToggle eklendi.

--- src/components/navi/Navi.js
+++ src/components/navi/Navi.js
@@ -1,22 +1,33 @@
-import React, { Component } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import { Collapse, Nav, Navbar, NavbarToggler } from "reactstrap";
+import { Collapse, Nav, Navbar, NavbarToggler, NavItem } from "reactstrap";
 
 const Navi = () => {
+  const [isActive, setActive] = useState(false);
+
+  const toggleClass = () => {
+    setActive(!isActive);
+  };
+
   return (
     <div>
       <Navbar color="light" expand="md" light className="navContainer">
         <Link to="/" className="n-link">
-          Simpsons
+          Characters
         </Link>
-        <Collapse navbar>
+        <NavbarToggler className="me-2" onClick={toggleClass} />
+        <Collapse navbar className={isActive ? "show" : ""}>
           <Nav className="menuBar" navbar>
-            <Link to="/" className="nav-link">
-              Home
-            </Link>
-            <Link to="/add-character" className="nav-link">
-              New Character
-            </Link>
+            <NavItem>
+              <Link to="/" className="nav-link">
+                Home
+              </Link>
+            </NavItem>
+            <NavItem>
+              <Link to="/add-character" className="nav-link">
+                New Character
+              </Link>
+            </NavItem>
           </Nav>
         </Collapse>
       </Navbar>
